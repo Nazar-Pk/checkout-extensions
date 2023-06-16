4158a657e415cedfba50b19581572dcae207a15d
Nazar P <nazar.polosmak@utdweb.team>
2023-06-16 15:11:04 +0300

    Work with upsell

--- extensions/upsell-app/src/index.jsx
+++ extensions/upsell-app/src/index.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
-import {
-  useExtensionApi,
-  render,
-  Banner,
-  useTranslate,
-} from '@shopify/checkout-ui-extensions-react';
+import React, {useState} from 'react';
+import {Banner, render, useApplyCartLinesChange, useCartLines,} from '@shopify/checkout-ui-extensions-react';
 
-render('Checkout::CartLines::RenderAfter', () => <App />);
+render('Checkout::CartLines::RenderAfter', () => <App/>);
 
 function App() {
-  const {extensionPoint} = useExtensionApi();
-  const translate = useTranslate();
-  return (
-    <Banner title="upsell-app">
-      {translate('welcome', {extensionPoint})}
-    </Banner>
-  );
+    // Set up the states
+    const [product, setProduct] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    // Get a reference to the function that will apply changes to the cart lines from the imported hook
+    const lines = useCartLines();
+    const applyCartLinesChange = useApplyCartLinesChange();
+
+    const id = "gid://shopify/ProductVariant/45036555469115";
+
+    async function addUpsell() {
+      return await applyCartLinesChange({
+          type: "addCartLine",
+          merchandiseId: id,
+          quantity: 1,
+        });
+    }
+
+    const isUpsellApplied = lines.some((item) => {
+      return item.merchandise.id === id;
+    });
+
+    if(!isUpsellApplied) {
+      addUpsell().then(r => {
+        console.log(r, 111)
+      });
+    }
+
+    return (
+        <Banner title="upsell-app">
+            sss
+        </Banner>
+    );
 }
