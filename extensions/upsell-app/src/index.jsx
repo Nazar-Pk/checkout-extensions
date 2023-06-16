c5c00a56ac5450b7996a21f001fd60f4a496b018
Nazar P <nazar.polosmak@utdweb.team>
2023-06-16 18:05:44 +0300

    Done upsell ext

--- extensions/upsell-app/src/index.jsx
+++ extensions/upsell-app/src/index.jsx
@@ -1,25 +1,31 @@
 import React, {useState} from 'react';
-import {Banner, render, useApplyCartLinesChange, useCartLines,} from '@shopify/checkout-ui-extensions-react';
+import {
+    Banner,
+    BlockStack,
+    render,
+    useApplyCartLinesChange,
+    useCartLines, useSettings,
+} from '@shopify/checkout-ui-extensions-react';
 
 render('Checkout::CartLines::RenderAfter', () => <App/>);
 
 function App() {
-    // Set up the states
-    const [product, setProduct] = useState([]);
-    const [loading, setLoading] = useState(false);
-
-    // Get a reference to the function that will apply changes to the cart lines from the imported hook
     const lines = useCartLines();
     const applyCartLinesChange = useApplyCartLinesChange();
+    const {variant, title, description, collapsible, status: merchantStatus} = useSettings();
+    const status = merchantStatus ?? 'info';
+    const [showBanner, setShowBanner] = useState(false);
 
-    const id = "gid://shopify/ProductVariant/45036555469115";
+    if (variant) {
+        return  null;
+    }
 
     async function addUpsell() {
-      return await applyCartLinesChange({
+       await applyCartLinesChange({
           type: "addCartLine",
-          merchandiseId: id,
+          merchandiseId: variant,
           quantity: 1,
-        });
+       });
     }
 
     const isUpsellApplied = lines.some((item) => {
@@ -28,13 +34,21 @@ function App() {
 
     if(!isUpsellApplied) {
       addUpsell().then(r => {
-        console.log(r, 111)
+          setShowBanner(true);
+
+          setTimeout(() => {
+              setShowBanner(false);
+          },5000)
       });
     }
 
     return (
-        <Banner title="upsell-app">
-            sss
-        </Banner>
+        <BlockStack>
+            {showBanner && (
+                <Banner title={title} status={status} collapsible={collapsible}>
+                    {description}
+                </Banner>
+            )}
+        </BlockStack>
     );
 }
